d3c71630b4ccc7675f6ed1e42ede30d03069e0a0
Richard Coy <rich.a.coy@gmail.com>
2014-03-03 15:25:40 -0500

    Override of files
    Works :)

--- js/main.js
+++ js/main.js
@@ -1,38 +1,54 @@
 $(document).ready(function(){
 	
-  	var interval = setInterval(slideForward, 5000);
-	var sliding = false;
+
+		var interval = setInterval(slideForward, 5000);
+	  	var sliding = false;
+	  	var window_focus = true;
 	
+		/*
+		 * When window loses focus stop animation from firing to prevent queue up issues
+		 */
+		
+		$(window).focus(function() {
+		  window_focus=true;
+		}).blur(function() {
+		  window_focus=false;
+		});
+
 
 	/*
 	 * Forward movement
 	 */
 	function slideForward() {
-		$('.slider').removeClass('slider__reverse');
-		//if(!sliding){
-			//sliding = true;
-			$('.js-last-up').addClass('js-off-left').removeClass('js-last-up');
-			$('.js-at-bat').addClass('js-last-up').removeClass('js-at-bat');
-			$('.js-on-deck').addClass('js-at-bat').removeClass('js-on-deck');
-			$('.js-in-the-hole').addClass('js-on-deck').removeClass('js-in-the-hole');	
-				
-			$('.js-off-left').addClass('js-off-right').removeClass('js-off-left');
-			$('.js-off-right').addClass('js-in-the-hole').removeClass('js-off-right');
-			//sliding = false;
-			//}
+		if(!sliding){
+			if(window_focus){
+				sliding = true;
+				$('.slider').removeClass('slider__reverse');
+				$('.js-last-up').addClass('js-off-left').removeClass('js-last-up');
+				$('.js-at-bat').addClass('js-last-up').removeClass('js-at-bat');
+				$('.js-on-deck').addClass('js-at-bat').removeClass('js-on-deck');
+				$('.js-in-the-hole').addClass('js-on-deck').removeClass('js-in-the-hole');	
+				$('.js-off-left').addClass('js-off-right').removeClass('js-off-left');
+				$('.js-off-right').addClass('js-in-the-hole').removeClass('js-off-right');
+				setTimeout(function(){sliding=false}, 1200);
+			}
+		}
 	}
 	
 	/*
 	 * Reverse movement
 	 */
-	function slideReverse() { 
-		$('.js-in-the-hole').addClass('js-off-right').removeClass('js-in-the-hole');
-		$('.js-on-deck').addClass('js-in-the-hole').removeClass('js-on-deck');
-		$('.js-at-bat').addClass('js-on-deck').removeClass('js-at-bat');
-		$('.js-last-up').addClass('js-at-bat').removeClass('js-last-up');
-		
-		$('.js-off-right').addClass('js-off-left').removeClass('js-off-right');
-		$('.js-off-left').addClass('js-last-up').removeClass('js-off-left');
+	function slideReverse() {
+		if(!sliding){
+			sliding = true;
+			$('.js-in-the-hole').addClass('js-off-right').removeClass('js-in-the-hole');
+			$('.js-on-deck').addClass('js-in-the-hole').removeClass('js-on-deck');
+			$('.js-at-bat').addClass('js-on-deck').removeClass('js-at-bat');
+			$('.js-last-up').addClass('js-at-bat').removeClass('js-last-up');
+			$('.js-off-right').addClass('js-off-left').removeClass('js-off-right');
+			$('.js-off-left').addClass('js-last-up').removeClass('js-off-left');
+			setTimeout(function(){sliding=false}, 1200);
+			}
 	}
 
 
